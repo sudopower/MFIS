8cf47a9006bc49f3b930bc44b854c61c30e360a1
sudopower <nkirandroid@gmail.com>
2019-01-26 09:56:52 +0530

    mapping instructions/link pending...

---
+++ auth/map_inst.php
@@ -0,0 +1,77 @@
+
+<!--SHOWS ON THE RIGHT WITH INST AND LINKS TO HELP MAPPING-->
+<html>                                                
+<head>
+
+<title>Mapping_Right</title>
+
+<style>
+	.stdbackground{background-color:#EEEEEE; color:#000000; font-family: verdana,arial, helvetica, sans-serif; font-size: 11px; }
+	.menumainheads{background-color:#D2D2D3; color:#000000; font-family: verdana,arial, helvetica, sans-serif; font-size: 11px; }
+	
+	a.menumainheadslink:link {font-weight: bold; color: #000000; font-size: 11px;  text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menumainheadslink:visited {font-weight: bold; color: #000000; font-size: 11px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif} 
+	a.menumainheadslink:visited:hover {font-weight: bold; color: #000000; font-size: 11px;text-decoration:underline; line-height:18px; font-family: verdana, arial, helvetica, sans-serif}	
+	a.menumainheadslink:hover {font-weight: bold; color: #000000; font-size: 11px;text-decoration:underline;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menumainheadslink:active {font-weight: bold; color: #000000; font-size: 11px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+
+	a.menusubheadslink:link {font-weight: normal; color: #000000; font-size: 10px;  text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menusubheadslink:visited {font-weight: normal; color: #000000; font-size: 10px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif} 
+	a.menusubheadslink:visited:hover {font-weight: normal; color: #000000; font-size: 10px; text-decoration:underline;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}	
+	a.menusubheadslink:hover {font-weight: normal; color: #000000; font-size: 10px; text-decoration:underline; line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menusubheadslink:active {font-weight: normal; color: #000000; font-size: 10px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+
+	a.menuactivesubheadslink:link {font-weight: bold; color: #000000; font-size: 11px;  text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menuactivesubheadslink:visited {font-weight: bold; color: #000000; font-size: 11px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif} 
+	a.menuactivesubheadslink:visited:hover {font-weight: bold; color: #000000; font-size: 11px; text-decoration:underline;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}	
+	a.menuactivesubheadslink:hover {font-weight: bold; color: #000000; font-size: 11px; text-decoration:underline; line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+	a.menuactivesubheadslink:active {font-weight: bold; color: #000000; font-size: 11px;   text-decoration:none;line-height:18px; font-family: verdana, arial, helvetica, sans-serif}
+
+</style>
+
+
+<link REL="stylesheet" HREF="http://katariaweb.com/FinnSys/includes/standard.css" TYPE="text/css">
+
+
+</head>
+
+<body leftmargin="0" topmargin="0" marginheight="0" marginwidth="0">
+<center>
+<form name=form1 action="" method="post">
+
+<table border=0 cellpadding=5 width="100%" height="100%">
+<tr bgcolor="#eeeeee" height="40">
+<td colspan=2 class=normal>
+	<h1>Search Investor</h1>
+</td>
+</tr>
+<tr bgcolor="#eeeeee"><td class=smaller valign=top>
+<b>Map new folio to an existing investor</b>
+<ul>
+	<li>click on the Investor name / Address / PAN number / Phone etc. on the left panel, to start your search.</li><br>&nbsp;
+	<li>the right panel will show all the matches found.</li><br>&nbsp;
+	<li>accept the investor by pressing the button <input type=button name="Dummy1" Value = "Map Investor to this Folio" STYLE="font-family:Arial; font-size:10px; background-color: #3787af; font-weight:bold;color:#ffffff; width:180; height:20;"></li>
+</ul>
+<b>Create a new investor</b>
+<ul>
+	<li>start your search.</li><br>&nbsp;
+	<li>incase the investor is not found, press the button <input type=button name="Dummy2" Value = "Create a New Investor" STYLE="font-family:Arial; font-size:10px; background-color: #3787af; font-weight:bold;color:#ffffff; width:180; height:20;"> to create a new investor with the details as shown on the left panel.</li><br>&nbsp;
+	<li>make changes to the investor details, if required.</li><br>&nbsp;
+	<li>assign web access, create login/password etc., if required.</li>
+</ul>
+<b>Update investor / folio details</b>
+<ul>
+	<li>for each existing folio, the investor and the folio details are shown on the right panel.</li><br>&nbsp;
+	<li>incase the investor's details have changed since the last update, press the button <input type=button name="Dummy3" Value = "Update Investor Details" STYLE="font-family:Arial; font-size:10px; background-color: #3787af; font-weight:bold;color:#ffffff; width:150; height:20;"> to update the investor's details.</li><br>&nbsp;
+	<li>incase the folio details have changed since the last update, press the button <input type=button name="Dummy4" Value = "Update Holding / Bank details" STYLE="font-family:Arial; font-size:10px; background-color: #3787af; font-weight:bold;color:#ffffff; width:180; height:20;"> to update the folio details.</li><br>&nbsp;
+	<li>incase no changes are required, move to the next folio on the left panel.</li>
+</ul>
+</td></tr>
+</table>
+
+
+
+</form>
+</center>
+</body>
+</html>
