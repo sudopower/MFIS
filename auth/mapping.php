2e6b27289d15927dd0d3c2dcf6564459510b6b94
sudopower <nkirandroid@gmail.com>
2019-01-26 18:18:30 +0530

    show mapped profiles

--- auth/mapping.php
+++ auth/mapping.php
@@ -1,6 +1,6 @@
 <?php 
 include('session.php');
-$q= 'SELECT foliochk,inv_name,address1,city,pincode from investors where pan_no in 
+$q= 'SELECT foliochk,inv_name,sch_name,address1,city,pincode from investors where pan_no in 
 (SELECT pangno from cm_karvy where datetime > CURRENT_DATE 
 UNION SELECT panno1 from cm_franklin where datetime > CURRENT_DATE 
 UNION SELECT pan_no from cm_cams where datetime > CURRENT_DATE ) or ac_no in 
@@ -10,4 +10,22 @@ UNION select accnt_no from cm_franklin where datetime > CURRENT_DATE) or inv_nam
 (select invname from cm_karvy where datetime > CURRENT_DATE 
 UNION SELECT inv_name from cm_cams where datetime > CURRENT_DATE 
 UNION select inv_name from cm_franklin where datetime > CURRENT_DATE)';
+$result=$db->query($q);
+echo '<table border=1><tr><th>Folio No</th><th>Investor Name</th><th>Scheme</th><th>Address</th><th>City</th><th>Pincode</th><th>Map Profile<hr>All<input id="all" type="checkbox" onclick="chk_all();"></th></tr>';
+while($row=$result->fetch_row()){
+    echo '<tr><td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.$row[3].'</td><td>'.$row[4].'</td><td>'.$row[5].'</td><td><input type=checkbox></td></tr>';
+}
+echo '</table>';
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>
+function chk_all(){
+    if($('#all').prop("checked") == false){
+    $("input[type=checkbox]").prop('checked', false);
+            }
+            else if($('#all').prop("checked") == true){
+               
+    $("input[type=checkbox]").prop('checked', true);
+            }
+}
+</script>
